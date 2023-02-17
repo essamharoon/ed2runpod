1901fb7570243f9e6c2dc033e57530f5834afdb6
Essam Haroon <eh4@me.com>
2023-02-17 19:51:02 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,22 +2,4 @@
 
 echo "pod started"
 
-if [[ $PUBLIC_KEY ]]
-then
-    mkdir -p ~/.ssh
-    chmod 700 ~/.ssh
-    cd ~/.ssh
-    echo $PUBLIC_KEY >> authorized_keys
-    chmod 700 -R ~/.ssh
-    cd /
-    service ssh start
-fi
 
-tensorboard --logdir /workspace/EveryDream2trainer/logs --host 0.0.0.0 &
-if [[ $JUPYTER_PASSWORD ]]
-then
-    cd /
-    jupyter lab --allow-root --no-browser --port=8888 --ip=* --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=/workspace
-else
-    sleep infinity
-fi
